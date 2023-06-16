a3c7ab1a157e38f9fdc84ca100b9ea7ddd7755aa
Tej Pochiraju <tej@iotready.co>
2023-06-16 16:26:40 +0530

    Flow to add note when closing a picklist

--- iotready_godesi/api.py
+++ iotready_godesi/api.py
@@ -1,22 +1,37 @@
 import frappe
 from iotready_godesi import picking, webutils, utils
 
+
 @frappe.whitelist()
 def get_picklists():
     return picking.get_picklists()
+
 
 @frappe.whitelist()
 def get_picklist_summary(picklist_id):
     return picking.get_picklist_summary(picklist_id)
 
+
 @frappe.whitelist()
 def submit_activity_form(**kwargs):
     return webutils.record_event(**kwargs)
+
 
 @frappe.whitelist()
 def get_crate_quantity(crate_id):
     return utils.get_crate_quantity(crate_id)
 
+
 @frappe.whitelist()
 def get_package_ids(picklist_id):
     return picking.get_package_ids(picklist_id)
+
+
+@frappe.whitelist()
+def is_picking_complete(picklist_id):
+    return picking.is_picking_complete(picklist_id)
+
+
+@frappe.whitelist()
+def mark_as_complete(picklist_id, note):
+    return picking.mark_as_complete(picklist_id, note)
--- iotready_godesi/picking.py
+++ iotready_godesi/picking.py
@@ -16,6 +16,7 @@ def get_picklists():
         "reference_type": doctype,
         "creation": [">=", last_date_to_fetch],
         "status": "Open",
+        "allocated_to": frappe.session.user,
     }
     picklists = frappe.get_all("ToDo", filters=filters, fields=["reference_name"])
     picklists = [x["reference_name"] for x in picklists]
@@ -25,11 +26,56 @@ def get_picklists():
 
 
 def get_picklist_summary(picklist_id):
-    return frappe.render_template("templates/includes/picklist_summary.html", {"picklist_id": picklist_id})
+    return frappe.render_template(
+        "templates/includes/picklist_summary.html", {"picklist_id": picklist_id}
+    )
+
 
 def get_package_ids(picklist_id):
-    package_ids = ["New"] + list({r.package_id for r in frappe.get_all("Crate Activity", filters={
+    package_ids = ["New"] + list(
+        {
+            r.package_id
+            for r in frappe.get_all(
+                "Crate Activity",
+                filters={
                     "activity": "Picking",
                     "picklist_id": picklist_id,
-                }, fields=["package_id"])})
+                },
+                fields=["package_id"],
+            )
+        }
+    )
     return package_ids
+
+
+def is_picking_complete(picklist_id):
+    """
+    Returns True if all items in the picklist have been picked.
+    """
+    picklist = frappe.get_doc("Pick List", picklist_id)
+    for item in picklist.locations:
+        if item.qty > item.picked_qty:
+            return False
+    return True
+
+
+def mark_as_complete(picklist_id, note):
+    """
+    Marks a picklist as complete.
+    """
+    picklist = frappe.get_doc("Pick List", picklist_id)
+    picklist.status = "Completed"
+    picklist.add_comment("Comment", text=note)
+    picklist.save()
+    picklist.flags.ignore_validate = True
+    picklist.submit()
+    # Also mark the ToDo as done.
+    todos = frappe.get_all(
+        "ToDo",
+        filters={"reference_name": picklist_id, "allocated_to": frappe.session.user},
+    )
+    for todo in todos:
+        todo = frappe.get_doc("ToDo", todo.name)
+        todo.status = "Closed"
+        todo.save()
+    return True
