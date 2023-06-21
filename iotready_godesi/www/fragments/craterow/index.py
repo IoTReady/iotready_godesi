4fe319ec81637394c7239af0ce2914ce6af3be01
Tej Pochiraju <tej@iotready.co>
2023-06-21 12:11:48 +0530

    Remove fragments

--- iotready_godesi/www/fragments/craterow/index.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from bb_fnv_frappe import webutils
-
-
-def get_context(context):
-    context.title = "Crate Row"
-    parameters = frappe.form_dict
-    crate_id = parameters.get("crate_id")
-    context.crate = webutils.get_crate_details(crate_id)
